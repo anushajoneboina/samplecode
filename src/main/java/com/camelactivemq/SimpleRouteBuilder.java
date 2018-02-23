e4366ce0deb3c6af70f61927cb35eb0ac0ba3c65
anushajoneboina <anujoneboina@gmail.com>
2018-02-22 17:50:46 -0800

    hi

---
+++ src/main/java/com/camelactivemq/SimpleRouteBuilder.java
@@ -0,0 +1,11 @@
+package com.camelactivemq;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class SimpleRouteBuilder extends RouteBuilder {
+	//configure route for jms component
+    @Override
+    public void configure() throws Exception {
+        from("file:C:/inputFolder").split().tokenize("\n").to("jms:queue:javainuse");
+    }
+}
